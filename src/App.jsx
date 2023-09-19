e5865c6bf44889fc98f78645498763e2c9d5167e
Lokendra Dangi <lokendra.dangi@himalaya-airlines.com>
2023-09-19 12:34:50 +0545

    starter project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="">
+      <h2 className="text-center text-xl font-bold my-10">
+        Tailwind CSS + VITE
+      </h2>
+    </div>
+  );
+}
+
+export default App;
